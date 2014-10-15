fe4eea1c094ee2221fbd511e42fdbfe92204f4a0
unknown <pavsi02@CZPRLOAN00.ca.com>
2014-10-15 11:22:16 +0200

    nahodna_cisla -> random_number chybove hlasky

--- src/masking/MaskerSettings.java
+++ src/masking/MaskerSettings.java
@@ -1,6 +1,7 @@
 package masking;
 
 import input.FileReader;
+import log.Logger;
 import masking.rules.*;
 //import masking.rules.NothingRule;
 
@@ -31,10 +32,16 @@ public class MaskerSettings extends Exception {
 				return new StarsRule();
 				
 
-			case "nahodna_cisla":
+			case "random_number":
 				if(numOfParams==2){
-					int min = Integer.parseInt(arrParams[1]);
-					int max = Integer.parseInt(arrParams[2]);
+					int min = 0;
+					int max = 0;
+					try{
+						min = Integer.parseInt(arrParams[1]);
+						max = Integer.parseInt(arrParams[2]);
+					}catch(Exception e){
+						Logger.log("Bad parametrs for random number");
+					}
 					return new RandomNumberRule(min,max);
 				}else{
 					return new RandomNumberRule();
@@ -42,8 +49,10 @@ public class MaskerSettings extends Exception {
 				
 			case "ReplaceFromSeedsFile" :
 				return new ReplaceRule(arrParams[1]);
+			default:
+				Logger.log("Bad format line in masking settings file. / "+s);
 		}
-		return null;
+		return new NothingRule();
 	}
 	
 	//rozdeli radek na jednotlive parametry (napr. random_number;4;9 => array("random_number","4","9"))
