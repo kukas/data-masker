7b5a045b66f82e99e75bb5a7b6ef8ac9d1a78af3
unknown <balji01@czprloan09.ca.com>
2014-10-15 13:34:37 +0200

    out.println -> logger.debug

--- src/logic/Logic.java
+++ src/logic/Logic.java
@@ -55,7 +55,8 @@ public class Logic {
 		Masker masker = new Masker(maskingSettingsFile);
 		String[] input;
 		String[][] database;
-		while((input = fReader.readNLines(lines))[0] != null){System.out.println("Masking "+input.length+" lines");
+		while((input = fReader.readNLines(lines))[0] != null){
+			Logger.debug("Masking "+input.length+" lines");
 			dReader.input = input;
 			database = dReader.read();
 			database = masker.mask(database);
