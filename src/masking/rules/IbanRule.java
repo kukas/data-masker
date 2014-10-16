c2884f4484e9454e965322bbadf441efad2fabd7
unknown <bouja03@CZPRLOAN04.ca.com>
2014-10-16 11:04:09 +0200

    IBAN pro vsechny zeme + seeds

--- src/masking/rules/IbanRule.java
+++ src/masking/rules/IbanRule.java
@@ -5,17 +5,35 @@ import java.math.BigInteger;
 import log.Logger;
 
 public class IbanRule implements MaskingRule{
-
+		
+	String countryCode ="";
+	boolean countryCodeSet = false;
+	
+	public IbanRule(String countryCode){
+		this.countryCode =  countryCode;
+		countryCodeSet = true;
+	}
+	public IbanRule(){
+	}
+	
 	@Override
 	public String mask(String in) {
 		String accNumber = in.substring(4);
 		RandomDigitRule randomizeAccNumber = new RandomDigitRule(true);
 		accNumber = randomizeAccNumber.mask(accNumber);
-		Logger.debug(accNumber);
-		String countryCode = "CZ";
+		//Logger.debug(accNumber);
+		
+		if(this.countryCode.length() != 2){
+			ReplaceRule country = new ReplaceRule("seeds/countryCodes.txt");
+			countryCode = country.mask("00");
+		}
+		
+		//ReplaceRule country = new ReplaceRule("seeds/countryCodes.txt");
+		
+		//countryCode = "CZ";
 		String countryCodeConverted = (((int)countryCode.charAt(0)) - 55) + "";
 		countryCodeConverted += (((int)countryCode.charAt(1)) - 55) + "";
-		Logger.debug(countryCodeConverted);
+		//Logger.debug(countryCodeConverted);
 		BigInteger check = new BigInteger(accNumber + countryCodeConverted);
 		int mod = (check.mod(new BigInteger("97"))).intValue();
 		mod = 98 - mod;
@@ -24,7 +42,13 @@ public class IbanRule implements MaskingRule{
 		checkDigit = "0"+ checkDigit;	
 		}
 		
+		
 		String out = countryCode + checkDigit + accNumber;
+		//Logger.debug(out);
+		if(!countryCodeSet){
+			countryCode= "";
+		}
+		
 		return out;
 	}
 
