8cf5f48f3583d22a0334d9d1f878e5962d63fba7
unknown <volva01@CZPRLOAN03.ca.com>
2014-10-17 14:03:30 +0200

    RYCHLE OPRAVIT STREDNIKY

--- src/output/SettingsWriter.java
+++ src/output/SettingsWriter.java
@@ -6,27 +6,27 @@ import java.io.IOException;
 import java.util.Vector;
 
 public class SettingsWriter {
-	
-	public void write(String address, Vector<Vector<Object>> data){
-		
+
+	public void write(String address, Vector<Vector<Object>> data) {
+
 		Vector arrayData[] = data.toArray(new Vector[0]);
-		
+
 		try {
 			BufferedWriter bw = new BufferedWriter(new FileWriter(address));
-			for(int i = 0; i < data.size(); i++){
+			for (int i = 0; i < data.size(); i++) {
 				String s = "";
-				for(int j = 0; j < data.get(i).size(); j++){
-				
-				if(data.get(i).get(j)==null){
-					s += "";
-				}else{
-					s += data.get(i).get(j);
-				}
-				
-				if(!(j == data.get(i).size() - 1)){
-					s+= ";";
-				}
-				
+				for (int j = 0; j < data.get(i).size(); j++) {
+
+					if (data.get(i).get(j) == null) {
+						s += "";
+					} else {
+						s += data.get(i).get(j).toString().replaceAll(";", ",");
+					}
+
+					if (!(j == data.get(i).size() - 1)) {
+						s += ";";
+					}
+
 				}
 				bw.write(s);
 				bw.newLine();
@@ -37,8 +37,6 @@ public class SettingsWriter {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
-	}	
+
+	}
 }
